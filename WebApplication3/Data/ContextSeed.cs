c0ba679b1aec3dd4f1d693795fd4889dd91e3ce4
unknown <locntgbh18350@fpt.edu.vn>
2023-03-11 00:39:45 +0700

    hi

---
+++ WebApplication3/Data/ContextSeed.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data
+{
+    public class ContextSeed
+    {
+        public static async Task SeedRolesAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Roles
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.QaManagment.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.QmManagment.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Staff.ToString()));
+        }
+        public static async Task AdminAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Default User
+            var defaultUser = new User
+            {
+                UserName = "admin",
+                Email = "locntgbh18350@fpt.edu.vn",
+                FirstName = "Loc",
+                LastName = "Nguyen",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "123Pa$$word.");
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Roles.Staff.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Roles.QmManagment.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Roles.QaManagment.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Roles.Admin.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ WebApplication3/Models/UserViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApplication3.Models
+{
+    public class UserViewModel
+    {
+        public string UserId { get; set; }
+        public string? LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? BirthPlace { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+        public uint Age { get { return (uint)(DateTime.Now.Year - DateOfBirth.Year); } }
+        public string FullName
+        {
+            get
+            {
+                var FullName = LastName + " " + FirstName;
+                return FullName;
+            }
+        }
+    }
+}
